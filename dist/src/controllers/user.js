c8e4d60e24169c57103e4848224c3ffbe38392b1
mustafa-mahmoud203 <must.afa000262@gmail.com>
2024-10-15 18:04:17 +0300

    [+] add get users query

--- dist/src/controllers/user.js
+++ dist/src/controllers/user.js
@@ -13,13 +13,86 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const apiError_1 = __importDefault(require("../utils/apiError"));
+const order_model_1 = __importDefault(require("../../database/models/order.model"));
 class UserController {
     users(req, res, next) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
+                const { minOrders, lat, lng, radius, daysRecency } = req.query;
+                let { category } = req.query;
+                if (!minOrders || !lat || !lng || !radius || !daysRecency) {
+                    return next(new apiError_1.default("All fields are required", 400));
+                }
+                if (!category)
+                    category = "Uncategorized";
+                const daysRecencyNum = parseInt(daysRecency, 10);
+                const startDay = new Date();
+                startDay.setDate(startDay.getDate() - daysRecencyNum);
+                const topUsers = yield order_model_1.default.aggregate([
+                    {
+                        $geoNear: {
+                            near: {
+                                type: "Point",
+                                coordinates: [
+                                    parseFloat(lng),
+                                    parseFloat(lat),
+                                ],
+                            },
+                            distanceField: "distance",
+                            maxDistance: parseFloat(radius),
+                            spherical: true,
+                        },
+                    },
+                    {
+                        $match: {
+                            date: { $gte: startDay },
+                        },
+                    },
+                    {
+                        $unwind: "$products",
+                    },
+                    {
+                        $match: {
+                            "products.category": category,
+                        },
+                    },
+                    {
+                        $group: {
+                            _id: "$user",
+                            totalOrders: { $sum: 1 },
+                            totalPrice: { $sum: "$totalPrice" },
+                        },
+                    },
+                    {
+                        $match: {
+                            totalOrders: { $gte: parseInt(minOrders, 10) },
+                        },
+                    },
+                    {
+                        $sort: {
+                            totalPrice: -1,
+                        },
+                    },
+                    {
+                        $limit: 5,
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            Name: "$_id.name",
+                            Email: "$_id.email",
+                            totalOrders: 1,
+                            totalPrice: 1,
+                        },
+                    },
+                ]);
+                if (!topUsers || topUsers.length === 0) {
+                    return next(new apiError_1.default("No users found", 404));
+                }
+                return res.status(200).json({ data: topUsers });
             }
             catch (error) {
-                throw new apiError_1.default(error.message, error.statusCode);
+                return next(new apiError_1.default(error.message, error.statusCode || 500));
             }
         });
     }
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,90 @@
 import { NextFunction, Request, Response } from "express";
 import ApiError from "../utils/apiError";
+import orderModel from "../../database/models/order.model";
 
 class UserController {
   public async users(req: Request, res: Response, next: NextFunction) {
     try {
+      const { minOrders, lat, lng, radius, daysRecency } = req.query;
+      let { category } = req.query;
+      if (!minOrders || !lat || !lng || !radius || !daysRecency) {
+        return next(new ApiError("All fields are required", 400));
+      }
+      if (!category) category = "Uncategorized";
+      const daysRecencyNum: number = parseInt(daysRecency as string, 10);
+      const startDay: Date = new Date();
+      startDay.setDate(startDay.getDate() - daysRecencyNum);
+
+      const topUsers = await orderModel.aggregate([
+        {
+          $geoNear: {
+            near: {
+              type: "Point",
+              coordinates: [
+                parseFloat(lng as string),
+                parseFloat(lat as string),
+              ],
+            },
+            distanceField: "distance",
+            maxDistance: parseFloat(radius as string),
+            spherical: true,
+          },
+        },
+        {
+          $match: {
+            date: { $gte: startDay },
+          },
+        },
+
+        {
+          $unwind: "$products",
+        },
+
+        {
+          $match: {
+            "products.category": category,
+          },
+        },
+
+        {
+          $group: {
+            _id: "$user",
+            totalOrders: { $sum: 1 },
+            totalPrice: { $sum: "$totalPrice" },
+          },
+        },
+
+        {
+          $match: {
+            totalOrders: { $gte: parseInt(minOrders as string, 10) },
+          },
+        },
+
+        {
+          $sort: {
+            totalPrice: -1,
+          },
+        },
+
+        {
+          $limit: 5,
+        },
+        {
+          $project: {
+            _id: 0,
+            Name: "$_id.name",
+            Email: "$_id.email",
+            totalOrders: 1,
+            totalPrice: 1,
+          },
+        },
+      ]);
+      if (!topUsers || topUsers.length === 0) {
+        return next(new ApiError("No users found", 404));
+      }
+      return res.status(200).json({ data: topUsers });
     } catch (error: any) {
-      throw new ApiError(error.message, error.statusCode);
+      return next(new ApiError(error.message, error.statusCode || 500));
     }
   }
 }
